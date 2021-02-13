312a2736ef813a1455fca271ffe67009cf855eed
jamesjoel <james.joelj@gmail.com>
2021-02-13 19:49:25 +0530

    angular day2

---
+++ ang-pro1/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Box1Component } from './box1/box1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Box1Component,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ang-pro1/src/app/box1/box1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'box1',
+  templateUrl: './box1.component.html',
+  styleUrls: ['./box1.component.scss']
+})
+export class Box1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
