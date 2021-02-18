f4f935eca214bdbf9d226122f83360b6b67c6d24
jamesjoel <james.joelj@gmail.com>
2021-02-18 19:58:57 +0530

    crud in angular

--- ang-pro1/src/app/pages/employee/employee.component.ts
+++ ang-pro1/src/app/pages/employee/employee.component.ts
@@ -33,6 +33,30 @@ export class EmployeeComponent implements OnInit {
       city : "mumbai",
       gender : "female",
       address : "new market"
+    },
+    {
+      _id : 3,
+      name : "gaurav",
+      age : 22,
+      city : "mumbai",
+      gender : "female",
+      address : "new market"
+    },
+    {
+      _id : 4,
+      name : "nilesh",
+      age : 22,
+      city : "mumbai",
+      gender : "female",
+      address : "new market"
+    },
+    {
+      _id : 5,
+      name : "nidhi",
+      age : 22,
+      city : "mumbai",
+      gender : "female",
+      address : "new market"
     }
   ]
 
@@ -52,10 +76,46 @@ export class EmployeeComponent implements OnInit {
     }
   }
   save(){
-    this.employes.push(this.emp);
+    if(this.emp._id){
+      for(let i = 0; i < this.employes.length; i++)
+      {
+        //console.log(this.employes[i]._id);
+        // console.log(this.emp._id);
+        if(this.employes[i]._id == this.emp._id)
+        {
+          this.employes[i] = this.emp;
+        }
+
+      }
+    }
+    else{
+
+      let total = this.employes.length;
+      this.emp._id = total+1;
+      this.employes.push(this.emp);
+    }
+  //  console.log(this.employes);
   }
   askDelete(obj){
     this.emp = obj;
   }
+  delete(){
+    let n = this.employes.indexOf(this.emp);
+    this.employes.splice(n, 1);
+  }
+  askEdit(obj){
+    // this.emp = obj;
+    this.emp = {... obj};
+  }
 
 }
+/*
+  arr = ["red", "green", "blue", "yellow"];
+  x = "green";
+
+  n = arr.indexOf(x);
+  arr.splice(n, 1);
+
+
+
+*/
