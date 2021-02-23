94aacffd9e9015e09985b809158a727c0bc41aa4
jamesjoel <james.joelj@gmail.com>
2021-02-23 19:49:40 +0530

    module in angular

---
+++ shopping-ang/src/app/admin/admin.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CategoryComponent } from './pages/category/category.component';
+
+
+@NgModule({
+  declarations: [AdminComponent, LoginComponent, ProductComponent, CategoryComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
---
+++ shopping-ang/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path : "",
+    loadChildren : ()=> import("./user/user.module").then(module1 => module1.UserModule)
+  },
+  {
+    path : "admin",
+    loadChildren : ()=> import('./admin/admin.module').then(module2 => module2.AdminModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+/*
+express().listen(3000, function()=>{
+
+})
+
+function demo(x){
+
+}
+req => req.body
+
+
+*/
